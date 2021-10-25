38c6b1f50cfcff6cfa4b7c2e78e92b1653682dd4
bygoolove <dbygoolove@gmail.com>
2021-10-25 19:49:42 +0400

    freecodecamp

--- freecodecamp.js
+++ freecodecamp.js
@@ -26,3 +26,12 @@ function testNotEqual(val) {
 }
 
 testNotEqual(10);
+
+function testStrictNotEqual(val) {
+  if (val !==17) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
