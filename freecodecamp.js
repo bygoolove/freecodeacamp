bd5aaed5d6e9b1f33baaa91fb8f420f5619766a3
bygoolove <dbygoolove@gmail.com>
2021-10-25 21:59:24 +0400

    freecodecamp

--- freecodecamp.js
+++ freecodecamp.js
@@ -124,3 +124,64 @@ function testLogicalOr(val) {
 }
 
 testLogicalOr(15);
+
+
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } 
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
+
+
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  else if (val < 5) {
+    return "Smaller than 5";
+  }else{
+    
+  }
+
+  return "Between 5 and 10";
+}
+
+testElseIf(7);
+
+
+
+function testSize(num) {
+  // Only change code below this line
+if(num<5){
+  return "Tiny"
+}else if (num<10){
+  return "Small"
+}else if (num<15){
+  return "Medium"
+}else if(num<20){
+  return "Large"
+  }else if(num>=20){
+    return"Huge"
+  }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+testSize(7);
+
+
