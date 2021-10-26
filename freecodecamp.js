475b96698d22d87b4111175959ee478db96049ce
bygoolove <dbygoolove@gmail.com>
2021-10-26 20:58:26 +0400

    freecodecamp

--- freecodecamp.js
+++ freecodecamp.js
@@ -291,3 +291,14 @@ var myDog = {
   
   // Only change code above this line
   };
+
+  var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  
+  var hatValue = testObj.hat;      // Change this line
+  var shirtValue = testObj.shirt;    // Change this line
