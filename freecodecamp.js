455572995d103595b4b90fc7fb4a1dce1207a359
bygoolove <dbygoolove@gmail.com>
2021-10-26 21:18:39 +0400

    freecodecamp

--- freecodecamp.js
+++ freecodecamp.js
@@ -313,3 +313,14 @@ var myDog = {
   
   var entreeValue = testObj["an entree"];   // Change this line
   var drinkValue = testObj ["the drink"];    // Change this line
+
+  var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line
+  
+  var playerNumber = 16;       // Change this line
+  var player = testObj[playerNumber]
